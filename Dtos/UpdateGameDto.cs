51b644444f8b4d96902c51343fa5c035abd78f7a
Cory Campbell <campbellcory2360@gmail.com>
2026-03-10 18:51:37 -0400

    Updated required fields in DTO

--- Dtos/UpdateGameDto.cs
+++ Dtos/UpdateGameDto.cs
@@ -1,8 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 namespace GameStore.Api.Dtos;
 
 public record UpdateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(50)] string Genre,
+    [Required][Range(1,100)] decimal Price,
     DateOnly ReleaseDate
 );
--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e802b3c9055b888f113e57751e0244c9972ae987")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
